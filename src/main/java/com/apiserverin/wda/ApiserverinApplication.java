cb056ce20b6e3e9bb9da20332a78d9bfc203be86
whydda <whydda@naver.com>
2018-06-19 23:01:46 +0900

    Initial commit

---
+++ src/main/java/com/apiserverin/wda/ApiserverinApplication.java
@@ -0,0 +1,12 @@
+package com.apiserverin.wda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiserverinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiserverinApplication.class, args);
+	}
+}
---
+++ src/test/java/com/apiserverin/wda/ApiserverinApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apiserverin.wda;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiserverinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
